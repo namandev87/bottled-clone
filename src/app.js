30a17f3d4ab20573248ba5adf03057b17f605902
Naman Tailor <namanisliving@gmail.com>
2026-09-16 20:26:45 +0530

    feat: add user retrieval endpoint

--- src/app.js
+++ src/app.js
@@ -31,14 +31,18 @@ app.post("/users", async (req, res) => {
 })
 
 app.get("/users/:userId", async (req, res) => {
-    const result = await pool.query(`SELECT username, email
+    try {
+        const result = await pool.query(`SELECT username, email
                                      FROM users
                                      WHERE id = $1`, 
                                      [req.params.userId]);
-    if(result.rows.length === 1){
-        res.status(200).send(result.rows[0]);
-    } else {
-        res.status(404).send("Not Found");
+        if(result.rows.length === 1){
+            res.status(200).send(result.rows[0]);
+        } else {
+            res.status(404).send("Not Found");
+        }
+    } catch(err){
+        return res.status(500).send("Error occured");
     }
 })
 
